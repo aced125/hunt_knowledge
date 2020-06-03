7886fc8d1877aa55e078963b4a1c624b1a5f0985
Laksh <laksh@genei.io>
2020-06-03 01:22:28 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aced125/hunt_knowledge',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
 # fmt: on
