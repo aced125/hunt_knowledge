f637f2cd68af0ba4dc770d015354e96ecb315a93
Laksh <laksh@genei.io>
2020-06-06 22:12:28 +0100

    Disable test w/o aws creds

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,12 @@
 from hunt_knowledge import utils
 
 
-def test_query_dynamodb_by_category():
-    response = utils.query_dynamo_by_category(
-        period=1, table_name="CuratedArticlesDB", category="pharma"
-    )
-    assert isinstance(response["Items"], list)
-    response_2 = utils.query_dynamo_by_category(
-        period=1, table_name="CuratedArticlesDB", category="hacker_news"
-    )
-    assert isinstance(response_2["Items"], list)
+# def test_query_dynamodb_by_category():
+#     response = utils.query_dynamo_by_category(
+#         period=1, table_name="CuratedArticlesDB", category="pharma"
+#     )
+#     assert isinstance(response["Items"], list)
+#     response_2 = utils.query_dynamo_by_category(
+#         period=1, table_name="CuratedArticlesDB", category="hacker_news"
+#     )
+#     assert isinstance(response_2["Items"], list)
